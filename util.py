b86c4a6f0644fd5869f551214e3c2955733a08e5
Alec <abarrett.wilsdon@gmail.com>
2022-06-22 00:15:10 +0200

    improved error logging for standardize_event

--- util.py
+++ util.py
@@ -51,7 +51,10 @@ def standardize_event(event):
 
     elif event.get("httpMethod") == "POST" and event.get("body") and "application/x-www-form-urlencoded" in ez_insensitive_get(event, "headers", "Content-Type", fallback_value="").lower() and "=" in event["body"]:  # POST from <form> -> synchronous API Gateway
         body_as_dict = {k:(v[0] if isinstance(v, list) and len(v)==1 else v) for k,v in parse_qs(event["body"]).items()} # v by default will be a list, but we extract the item if its a one-item list
-        event.update(body_as_dict)
+        if isinstance(body_as_dict, dict):
+            event.update(body_as_dict)
+        else:
+            logging.error(f"Malformed POST body received by standardize_event. Likely due to list, rather than dict, at top level of body JSON. body_as_dict is of type {type(body_as_dict)}")
 
     elif event.get("httpMethod") == "POST" and event.get("body"):  # POST, synchronous API Gateway
         if isinstance(event['body'], dict):
