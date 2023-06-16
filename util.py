254204a24284de50ee84972b42d873c5e3c85e7c
Alec <abarrett.wilsdon@gmail.com>
2023-06-16 12:35:02 -0600

    several upgrdaes to detect_and_convert_datetime_str - more locales, more selectors, more LIST_OF_LOCALE_DT_FORMATS, better logging, and more

--- util.py
+++ util.py
@@ -813,13 +813,16 @@ def format_timestamp(timestamp, **kwargs):
 
     [ ] "1.1.7"   # unclear if month or day first, waiting for another example
     
-    [ ] "Avril 2016"   # 10 len, diff selector
-    [ ] '31 janv. 2023' - 13 len, diff selector
-    [ ] '16-5 月-2023' - has weird-ass selector
-    [ ] '12-май-2023', '26 maj 2023', '21-фев-2023', '31-авг-2022', '05-Nis-2023', '27-Lut-2023', '06-апр-2023', '20-Dic-2022', '22-Ene-2021' - juts need locale
+    Haven't bothered with locale and/or len yet:
+        [ ] '27. syyskuuta 2022' - 'fi_FI', 18 len
+        [ ] '26 maj 2023', - 'sv_SE'
+        [ ] '05-Nis-2023' - 'tr_TR'
+        [ ] '16-5 月-2023' - has weird-ass selector. chinese?
+        [ ] '31 janv. 2023' - must be some other locale? won't work with 'fr_FR' even though it should
+    
+    [ ] 'Sept. 28, 2022' - the 't' in 'Sept' rather than 'Sep' makes it not match
+
     [ ] '18- Apr-2023' - dumbass internal space
-    [ ] '27. syyskuuta 2022'
-    [ ] '12 januari 2023'
 
     [ ] "Mon May 10 2021 18:24:31 GMT+0000 (Coordinated Universal Time)"   # tried  "%a %B %d %Y %H:%M:%S %Z%z", didnt work. don't know how to handle (Coordinated Universal Time)
     [ ] 2021-06-17T11:46:24-05 # needs two trailing 0's
@@ -829,16 +832,24 @@ def format_timestamp(timestamp, **kwargs):
     [ ] '2023a4m24j', '2023a1m23j'
     [ ] dumb af, not going to add - '06 Jun 2023 2023 (4:55)'
 """
+def standardize_dt_str_to_utc(standard_dt_str, **kwargs):
+    try:
+        output_dt = datetime.utcfromtimestamp(calendar.timegm(standard_dt_str)) # convert from time.struct_time to datetime.date
+        return datetime.strftime(output_dt, kwargs.get("output_format", "%Y-%m-%d %H:%M:%S"))
+    except:
+        logging.warning("Some error while trying to standardize datetime_str to UTC - {standard_dt_str}")
+        return kwargs.get("null_value", "")
+
 def detect_and_convert_datetime_str(datetime_str, **kwargs):
     LIST_OF_DT_FORMATS = ["%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%S%z", "%Y-%m-%dT%H:%M:%S", "%Y-%m-%d %H:%M:%SZ", "%Y-%m-%d %H:%M:%S %Z", "%Y-%m-%d %H:%M:%ST%z", "%Y-%m-%d %H:%M:%S %z %Z", "%Y-%m-%d %H:%M:%S", "%Y-%m-%dT%H:%M:%S.%f%z", "%a, %d %b %Y %H:%M:%S %Z", "%a %b %d, %Y", "%m/%d/%Y %H:%M:%S %p", "%A, %B %d, %Y, %H:%M %p",  "%Y-%m-%dT%H:%M:%S.%fZ", "%Y-%m-%dT%H:%M:%S.%f", "%Y-%m-%dT%H:%M:%S.SSSZ", "%a %b %d %Y %H:%M:%S %Z%z", "%b %d, %Y", '%d-%b-%Y', '%Y/%m/%d', "%Y-%m-%dT%H:%M:%S %Z", "%a, %m/%d/%Y - %H:%M", "%B, %Y",  "%Y-%m-%d %H:%M:%S %z", "%a, %d %b %Y %H:%M:%S %z", "%a, %d %b %Y %H:%M:%S", "%B %d, %Y", "%B %Y", "%Y-%m", "%Y-%m-%dT%H:%M:%ST%z", "%A, %d-%B-%Y %H:%M:%S %Z", "%Y", "%Y-%m-%d @ %H:%M:%S %Z", "%Y-%m-%dT%H:%M%z", "%Y-%m-%d %H:%M:%S %z %Z", "%a, %d %b %Y %H:%M:%S%Z", '%a, %d %b %Y %H:%M:%S %z %Z', '%A, %d-%b-%Y %H:%M:%S %Z', "%Y-%m-%d T %H:%M:%S %z", '%Y-%m-%d %H:%M:%S.%f', "%m/%d/%y %H:%M",  "%a %d %b %H:%M", "%Y-%m-%dT%H:%M", "%b %d %Y %H:%M:%S", "%A, %B %d, %Y %H:%M %p", "%Y-%m-%d@%H:%M:%S %Z", "%m/%d/%Y %H:%M %p %Z", "%a, %b %d", "%A, %B %d, %Y", "%Y-%m-%d", "%Y %m %d", '%a %b %d %H:%M:%S %Z %Y', '%a %b %d %H:%M:%S %z %Y', '%d %b %Y %H:%M %p', '%d %b %Y', '%a, %d %b %y %H:%M:%S %z', '%dst %B, %Y', '%dnd %B, %Y', '%drd %B, %Y', '%dth %B, %Y', '%b %d %Y', '%b %d, %Y, %I:%M:%S %p', '%m/%d/%y, %I:%M:%S %p', '%d/%m/%Y, %I:%M:%S %p', '%d %b. %Y', '%d/%b/%Y', '%B %d, %Y %I:%M %p', '%d-%b-%Y, %I:%M:%S %p', '%d/%b/%Y, %I:%M:%S %p', '%m/%d/%Y, %I:%M:%S %p', '%b-%d-%Y', '%Y-%m-%d %H:%M:%ST23:59', '%d %b %Y, %I:%M %p', '%d %b %Y %H:%M', '%B %d, %Y (%I:%M %p)', '%b. %d, %Y', '%d/%b/%y', '%Y-%m-%d@%H:%M:%S', '%d %b %Y %H:%M:%S %z', '%d/%b/%y, %I:%M:%S %p', '%b %d %Y %I:%M %p', '%b %d, %Y %I:%M %p', '%A %d %B, %Y %I:%M %p', '%A, %d %b %Y', '%a, %d %b %Y %H:%M:%S %z', '%d. %B %Y', '%d %B %Y', '%d-%b-%y', '%B %dst, %Y', '%B %dnd, %Y', '%B %drd, %Y', '%B %dth, %Y', '%d %B %Y (%I:%M %p)', '%b %d, %Y %H:%M']
-    LIST_OF_LOCALE_FORMATS = ['fr_FR', 'de_DE', 'it_IT', 'pl_PL', 'hr_HR'] # 'zh_CN',
-    LIST_OF_LOCALE_DT_FORMATS = ['%d-%B-%Y', '%d-%b-%Y', '%d %B %Y', '%d %b %Y']
+    LIST_OF_LOCALE_FORMATS = ['fr_FR', 'de_DE', 'it_IT', 'es_ES', 'nl_NL', 'da_DK', 'ru_RU', 'pl_PL', 'hr_HR'] # 'zh_CN',
+    LIST_OF_LOCALE_DT_FORMATS = ['%d-%B-%Y', '%d-%b-%Y', '%d %B %Y', '%d %b %Y', '%d %b. %Y', '%d. %B %Y', '%B %Y']
     locale.setlocale(locale.LC_TIME, "")
 
     if kwargs.get("country") and kwargs['country'] != 'United States':
-        LIST_OF_DT_FORMATS[20:20] = ['%d/%m/%Y', '%m/%d/%Y', '%d/%m/%y', '%m/%d/%y', '%d-%m-%Y', '%m-%d-%Y', '%d-%m-%y', '%m-%d-%y', '%d.%m.%Y', '%m.%d.%Y', '%d.%m.%y', '%m.%d.%y', '%d/%m/%Y, %H:%M', '%m/%d/%Y, %H:%M', '(%d/%m/%Y)', '(%m/%d/%Y)', '%d/%m/%Y %I:%M %p', '%m/%d/%Y %I:%M %p', '%d-%m-%Y, %I:%M:%S %p', '%m-%d-%Y, %I:%M:%S %p', '%Y-%d-%mT%H:%M:%S.%fZ', '%Y-%m-%dT%H:%M:%S.%fZ'] # 20:20 notion means insert the new list into the existing list, starting at index 20. this slightly speeds up processing of datetime_strs that match those selectors
+        LIST_OF_DT_FORMATS[20:20] = ['%d/%m/%Y', '%m/%d/%Y', '%d/%m/%y', '%m/%d/%y', '%d-%m-%Y', '%m-%d-%Y', '%d-%m-%y', '%m-%d-%y', '%d.%m.%Y', '%m.%d.%Y', '%d.%m.%y', '%m.%d.%y', '%d/%m/%Y, %H:%M', '%m/%d/%Y, %H:%M', '(%d/%m/%Y)', '(%m/%d/%Y)', '%d/%m/%Y %I:%M %p', '%m/%d/%Y %I:%M %p', '%d-%m-%Y, %I:%M:%S %p', '%m-%d-%Y, %I:%M:%S %p', '%Y-%d-%mT%H:%M:%S.%fZ', '%Y-%m-%dT%H:%M:%S.%fZ', '%Y-%d-%m, %I:%M:%S %p', '%Y-%m-%d, %I:%M:%S %p'] # 20:20 notion means insert the new list into the existing list, starting at index 20. this slightly speeds up processing of datetime_strs that match those selectors
     else: 
-        LIST_OF_DT_FORMATS[20:20] = ['%m/%d/%Y', '%d/%m/%Y', '%m/%d/%y', '%d/%m/%y', '%m-%d-%Y', '%d-%m-%Y', '%m-%d-%y', '%d-%m-%y', '%m.%d.%Y', '%d.%m.%Y', '%m.%d.%y', '%d.%m.%y', '%m/%d/%Y, %H:%M', '%d/%m/%Y, %H:%M', '(%m/%d/%Y)', '(%d/%m/%Y)', '%m/%d/%Y %I:%M %p', '%d/%m/%Y %I:%M %p', '%m-%d-%Y, %I:%M:%S %p', '%d-%m-%Y, %I:%M:%S %p', '%Y-%m-%dT%H:%M:%S.%fZ', '%Y-%d-%mT%H:%M:%S.%fZ'] 
+        LIST_OF_DT_FORMATS[20:20] = ['%m/%d/%Y', '%d/%m/%Y', '%m/%d/%y', '%d/%m/%y', '%m-%d-%Y', '%d-%m-%Y', '%m-%d-%y', '%d-%m-%y', '%m.%d.%Y', '%d.%m.%Y', '%m.%d.%y', '%d.%m.%y', '%m/%d/%Y, %H:%M', '%d/%m/%Y, %H:%M', '(%m/%d/%Y)', '(%d/%m/%Y)', '%m/%d/%Y %I:%M %p', '%d/%m/%Y %I:%M %p', '%m-%d-%Y, %I:%M:%S %p', '%d-%m-%Y, %I:%M:%S %p', '%Y-%m-%dT%H:%M:%S.%fZ', '%Y-%d-%mT%H:%M:%S.%fZ', '%Y-%m-%d, %I:%M:%S %p', '%Y-%d-%m, %I:%M:%S %p'] 
 
     if not datetime_str:
         return kwargs.get("null_value", "")
@@ -867,32 +878,26 @@ def detect_and_convert_datetime_str(datetime_str, **kwargs):
             standard_dt_str = datetime.utctimetuple(dt_str) # convert to UTC
             break
         except:
-            if dt_format == LIST_OF_DT_FORMATS[-1] and len(datetime_str) not in [10, 11, 12]: # if none matched
+            if dt_format == LIST_OF_DT_FORMATS[-1] and len(datetime_str) not in [10, 11, 12, 13, 14, 15, 16]: # if none matched
                 logging.warning(f"The datetime_str {datetime_str} (len {len(datetime_str)}, type {type(datetime_str)}) did not match any pattern")
                 return kwargs.get("null_value", "")
             elif dt_format == LIST_OF_DT_FORMATS[-1]:
                 for dt_locale in LIST_OF_LOCALE_FORMATS:
-                    try:
-                        locale.setlocale(locale.LC_TIME, dt_locale)
-                        for dt_format in LIST_OF_LOCALE_DT_FORMATS:
-                            try:
-                                dt_str = datetime.strptime(datetime_str, dt_format)
-                                locale.setlocale(locale.LC_TIME, "")
-                                standard_dt_str = datetime.utctimetuple(dt_str) # convert to UTC
-                                break
-                            except:
-                                pass
-                    except:
-                        if dt_locale == LIST_OF_LOCALE_FORMATS[-1]: # if none matched
+                    locale.setlocale(locale.LC_TIME, dt_locale)
+                    for dt_format in LIST_OF_LOCALE_DT_FORMATS:
+                        try:
+                            dt_str = datetime.strptime(datetime_str, dt_format)
+                            standard_dt_str = datetime.utctimetuple(dt_str) # convert to UTC
                             locale.setlocale(locale.LC_TIME, "")
-                            logging.warning(f"The datetime_str {datetime_str} (len {len(datetime_str)}, type {type(datetime_str)}) did not match any pattern")
-                            return kwargs.get("null_value", "")
+                            return standardize_dt_str_to_utc(standard_dt_str, **kwargs)
+                        except:
+                            if dt_format == LIST_OF_LOCALE_DT_FORMATS[-1] and dt_locale == LIST_OF_LOCALE_FORMATS[-1]: # if none matched
+                                locale.setlocale(locale.LC_TIME, "")
+                                logging.warning(f"The datetime_str {datetime_str} (len {len(datetime_str)}, type {type(datetime_str)}) did not match any pattern")
+                                return kwargs.get("null_value", "")
 
-    try:
-        output_dt = datetime.utcfromtimestamp(calendar.timegm(standard_dt_str)) # convert from time.struct_time to datetime.date
-        return datetime.strftime(output_dt, kwargs.get("output_format", "%Y-%m-%d %H:%M:%S"))
-    except:
-        return kwargs.get("null_value", "")
+
+    return standardize_dt_str_to_utc(standard_dt_str, **kwargs)
 
 
 ############################################# ~ List/Dict handling ~ ##########################################################
